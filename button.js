a8487394a937243e6fdece26b841fa9482098238
PelzaK <pelzak1@o2.pl>
2018-03-11 19:35:53 +0100

    button-input for UNIV-3.1.X support

--- button.js
+++ button.js
@@ -40,21 +40,31 @@
                 return;
 
             switch (hapcanMessage.frame[8]){
-				case 0x00: hapcanMessage.state = "OPEN";break;
+                case 0x00: hapcanMessage.state = "OPEN";break;
+                case 0x01: hapcanMessage.state = "DISABLED"; break;
 				case 0xFF: hapcanMessage.state = "CLOSED";break;
-				case 0xFE: hapcanMessage.state = "CH400ms";break;
-				case 0xFD: hapcanMessage.state = "CH4s";break;
-				case 0xFC: hapcanMessage.state = "CO400ms";break;
-				case 0xFB: hapcanMessage.state = "CO400-4";break;
-				case 0xFA: hapcanMessage.state = "CO4s";break;
+				case 0xFE: hapcanMessage.state = "CLOSED_HELD_400";break;
+				case 0xFD: hapcanMessage.state = "CLOSED_HELD_4000";break;
+				case 0xFC: hapcanMessage.state = "CLOSED_OPENED_400";break;
+				case 0xFB: hapcanMessage.state = "CLOSED_OPENED_400_4000";break;
+				case 0xFA: hapcanMessage.state = "CLOSED_OPENED_4000";break;
                 default: 
-                    node.warn('Button state value unknown:' + hapcanMessage.frame[9]);
+                    node.warn('Button state value unknown:' + hapcanMessage.frame[8]);
                     hapcanMessage.state = 'undefined';
 				
-			}
+            }
+            
+            hapcanMessage.channel = hapcanMessage.frame[7];
+            switch( hapcanMessage.frame[9] )
+            {
+                case 0x00 : hapcanMessage.led = 'OFF'; break;
+                case 0x01 : hapcanMessage.led = 'DISABLED'; break;
+                case 0xFF : hapcanMessage.led = 'ON'; break;
+                default:
+                    node.warn('Led value unknown:' + hapcanMessage.frame[9]);
+                    hapcanMessage.led = 'undefined';
+            }
 
-			hapcanMessage.channel = hapcanMessage.frame[7];
-			
             node.send({topic: 'Button message', payload: hapcanMessage});
         });
 
