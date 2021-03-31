924b185cdeeb032ad657aabda67bde4d585daa0e
mariusz-n <Mariusz.Niewczas@insert.com.pl>
2021-03-31 21:09:43 +0200

    Bugfix: temperature two's complement convertion

--- temp.js
+++ temp.js
@@ -1,43 +1,52 @@
- module.exports = function(RED) {
+module.exports = function (RED) {
 
     function TempInputNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
 
         node.gateway = RED.nodes.getNode(config.gateway);
         node.group = config.group;
         node.node = config.node;
         node.name = config.name;
-        
-        this.status({fill: "grey", shape: "dot", text: "not connected"});
 
-        node.gateway.eventEmitter.on('statusChanged', function(data){
+        this.status({ fill: "grey", shape: "dot", text: "not connected" });
+
+        node.gateway.eventEmitter.on('statusChanged', function (data) {
             node.status(data)
         })
 
-        node.gateway.eventEmitter.on('messageReceived_304', function(data){
-            
+        node.gateway.eventEmitter.on('messageReceived_304', function (data) {
+
             var hapcanMessage = data.payload;
 
-            if(hapcanMessage.node != node.node || hapcanMessage.group != node.group )
+            if (hapcanMessage.node != node.node || hapcanMessage.group != node.group)
                 return;
-            
+
             hapcanMessage.type = hapcanMessage.frame[7];
 
-            if( hapcanMessage.type !== 0x11 && hapcanMessage.type !== 0x01)
+            if (hapcanMessage.type !== 0x11 && hapcanMessage.type !== 0x01)
                 return;
 
-            hapcanMessage.temp = Number(Number(((hapcanMessage.frame[8] * 256) + hapcanMessage.frame[9]) * 0.0625).toFixed(1));
-            hapcanMessage.setpoint = Number(Number(((hapcanMessage.frame[10] * 256) + hapcanMessage.frame[11]) * 0.0625).toFixed(2));
-            hapcanMessage.hysteresis = Number(Number(hapcanMessage.frame[12] * 0.0625).toFixed(4));
+            hapcanMessage.temp = Number(decodeTwosComplement(hapcanMessage.frame[8], hapcanMessage.frame[9]) * 0.0625);
+            hapcanMessage.setpoint = Number(decodeTwosComplement(hapcanMessage.frame[10], hapcanMessage.frame[11]) * 0.0625);
+            hapcanMessage.hysteresis = Number((hapcanMessage.frame[12] + 1) * 0.0625);
 
-            node.send({topic: 'Temperature sensor message', payload: hapcanMessage});
+            node.send({ topic: 'Temperature sensor message', payload: hapcanMessage });
         });
 
-        this.on('close', function() {
+        this.on('close', function () {
             // tidy up any state
         });
 
     }
-    RED.nodes.registerType("temp-input",TempInputNode);
+
+    function decodeTwosComplement(msb, lsb) {
+        const value = (msb << 8) | lsb;
+        if (value & 0x8000) {
+            return -Number((~value & 0x7FFF) + 1);
+        }
+        return Number(value);
+    }
+
+    RED.nodes.registerType("temp-input", TempInputNode);
 }
